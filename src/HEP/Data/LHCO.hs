7599f572cbb0740010193f199ed88b7e811aafdc
cbpark <cbpark@gmail.com>
2014-09-04 21:53:17 +0200

    Add types for LHCO events

--- src/HEP/Data/LHCO.hs
+++ src/HEP/Data/LHCO.hs
@@ -1,1 +1,50 @@
+{-# LANGUAGE TemplateHaskell #-}
+
 module HEP.Data.LHCO where
+
+import           Control.Lens
+import           Data.IntMap  (IntMap)
+
+data Header = Header { eveNum      :: Int -- ^ event number.
+                     , triggerWord :: Int -- ^ triggering information.
+                     } deriving Show
+
+data Object = Object { -- | counter that labels the object.
+                       _counter :: Int
+                       -- | type of object.
+                       --
+                       --     * 0 = photon
+                       --     * 1 = electron
+                       --     * 2 = muon
+                       --     * 3 = hadronically-decaying tau
+                       --     * 4 = jet
+                       --     * 6 = missing transverse energy
+                     , _typ     :: Int
+                       -- | pseudorapidity.
+                     , _eta     :: Double
+                       -- | azimuthal angle.
+                     , _phi     :: Double
+                       -- | transverse momentum.
+                     , _pt      :: Double
+                       -- | invariant mass of the object.
+                       --
+                       -- For a jet, it is constructed from all energy and
+                       -- momentum that are contained within it.
+                     , _jmass   :: Double
+                       -- | number of tracks associated with the object.
+                       --
+                       -- In the case of a lepton, the number is multiplied by
+                       -- the charge of the lepton.
+                     , _ntrk    :: Double
+                       -- | either 1 or 2 for a jet that has been tagged as
+                       -- containing a b-quark.
+                     , _btag    :: Double
+                       -- | ratio of the hadronic /vs/ electromagnetic energy
+                       -- deposited in the calorimeter cells.
+                     , _hadem   :: Double
+                     } deriving Show
+
+makeLenses ''Object
+
+type Objects = IntMap Object
+type Event = (Header, Objects)
