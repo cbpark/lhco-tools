a7446ef3664a13597d5ed91824ccebf81c282e1a
cbpark <cbpark@gmail.com>
2014-09-12 21:26:34 +0200

    Export HasFourMomentum via HEP.Data.LHCO

--- src/HEP/Data/LHCO.hs
+++ src/HEP/Data/LHCO.hs
@@ -4,6 +4,7 @@ module HEP.Data.LHCO
        (
          module HEP.Data.LHCO.Type
        , module HEP.Data.LHCO.Parser
+       , module HV
 
        , numPhoton
        , numElectron
@@ -16,6 +17,8 @@ module HEP.Data.LHCO
 
 import           Control.Monad              (liftM)
 import           Control.Monad.Trans.Reader (Reader, ask)
+
+import           HEP.Vector                 as HV (HasFourMomentum (..))
 
 import           HEP.Data.LHCO.Parser
 import           HEP.Data.LHCO.Type
