38bd89638af84cc222ebe74f563b9e1a751740cf
cbpark <cbpark@gmail.com>
2015-10-06 21:44:20 +0900

    missingET gives TransverseMomentum

--- src/HEP/Data/LHCO.hs
+++ src/HEP/Data/LHCO.hs
@@ -6,6 +6,8 @@ module HEP.Data.LHCO
        , module LP
        , module LPU
        , module HK
+       , module LV
+       , module V2
 
        , numPhoton
        , numElectron
@@ -16,14 +18,16 @@ module HEP.Data.LHCO
        , missingET
        ) where
 
-import           Control.Monad              (liftM)
-import           Control.Monad.Trans.Reader (Reader, ask)
+import           Control.Monad                       (liftM)
+import           Control.Monad.Trans.Reader          (Reader, ask)
 
-import           HEP.Kinematics             as HK
+import           HEP.Kinematics                      as HK
+import           HEP.Kinematics.Vector.LorentzVector as LV (setEtaPhiPtM)
+import           HEP.Kinematics.Vector.TwoVector     as V2 (setPtPhi)
 
-import           HEP.Data.LHCO.Parser       as LP
-import           HEP.Data.LHCO.PipesUtil    as LPU
-import           HEP.Data.LHCO.Type         as LT
+import           HEP.Data.LHCO.Parser                as LP
+import           HEP.Data.LHCO.PipesUtil             as LPU
+import           HEP.Data.LHCO.Type                  as LT
 
 numObjs :: (Event -> [PhyObj a]) -> (PhyObj a -> Bool) -> Reader Event Int
 numObjs self cutf = liftM (length . filter cutf . self) ask
@@ -46,7 +50,7 @@ numJet = numObjs jet
 numBjet :: (PhyObj Bjet -> Bool) -> Reader Event Int
 numBjet = numObjs bjet
 
-missingET :: Reader Event Double
+missingET :: Reader Event TransverseMomentum
 missingET = do e <- ask
-               let ObjMet (_, a) = met e
-               return a
+               let ObjMet (phi', pt') = met e
+               return (setPtPhi pt' phi')
