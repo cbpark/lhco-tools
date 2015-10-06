2caac68589b22cebb7a2c4f8b36715b01b95f983
cbpark <cbpark@gmail.com>
2015-10-06 17:38:53 +0900

    Add helper functions to PipesUtil

--- src/HEP/Data/LHCO/PipesUtil.hs
+++ src/HEP/Data/LHCO/PipesUtil.hs
@@ -1,9 +1,16 @@
-module HEP.Data.LHCO.PipesUtil (getLHCOEvent) where
+module HEP.Data.LHCO.PipesUtil
+       (
+         getLHCOEvent
+       , eventFromHandle
+       , eventFromBS
+       ) where
 
 import           Control.Monad.Trans.State.Strict
 import           Data.ByteString.Char8            (ByteString)
 import           Pipes
 import           Pipes.Attoparsec                 (parse)
+import           Pipes.ByteString                 (fromHandle)
+import           System.IO                        (Handle)
 
 import           HEP.Data.LHCO.Parser             (lhcoEvent)
 import           HEP.Data.LHCO.Type               (Event)
@@ -12,3 +19,9 @@ getLHCOEvent :: Monad m => Producer ByteString m () -> Producer Event m ()
 getLHCOEvent s = do (r, s') <- lift $ runStateT (parse lhcoEvent) s
                     case r of Just (Right ev) -> yield ev >> getLHCOEvent s'
                               _               -> return ()
+
+eventFromBS :: Monad m => ByteString -> Producer Event m ()
+eventFromBS = getLHCOEvent . yield
+
+eventFromHandle :: MonadIO m => Handle -> Producer Event m ()
+eventFromHandle = getLHCOEvent . fromHandle
